fd05c5494285724395cd1e42c3f127b98e465eb4
yugi2004 <YUGI2004@GMAIL.COM>
2016-02-23 13:02:15 -0800

    Hello World app
    first appp Java , show comment in screen

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+// First java program
+// Chuong trinh hoc java , bai dau tien 
+// CMPR112 Huynh_Durendal
+/*
+   Printing a line of text with multiple statements.
+   Chuong trinh in  chu ra man hinh. Viet java dung notepad viet la chinh.
+*/
+
+public class HelloWorld
+{ 
+	public static void main(String[] args)
+	{
+	   // Notice: Class name as file name
+		System.out.println("Hello World");// cấu trúc câu để in ra màn hinh  . Thông qua truy xuất hàm hệ thống system, xuất out, in ra màn hinh println
+		/*
+		notic :
+		\n : newline
+		\t : Horizontal tab. tuc la tao khoang cach 1 cai tab
+		\r : Carriage return. Thể hiện các chữ cái phía sau \r là sẽ đc đem lên đầu cáu hiện tại và ghi đè lên dự liệu đã có.
+		\\: thể hiện backslash "\"
+		\" : thể hiện "
+		*/
+	}// end method Main
+}// end class HelloWorld
