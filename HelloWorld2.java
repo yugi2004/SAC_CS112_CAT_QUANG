e1505576cdacac3d57f992afce3f71e0da47f5e2
yugi2004 <YUGI2004@GMAIL.COM>
2016-02-24 20:51:59 -0800

    Lap4 Week3

---
+++ HelloWorld2.java
@@ -0,0 +1,9 @@
+public class HelloWorld2
+{
+	public static void main(String[] args){
+		String greeting;
+		greeting="Hello World!";
+	//	greeting=greeting.concat("Good Day");	test concat	
+		System.out.println(greeting);
+	}
+}
