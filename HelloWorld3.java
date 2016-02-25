74c1fae2f2a60e1372a02c0cdf6ad2df4d984e7e
yugi2004 <YUGI2004@GMAIL.COM>
2016-02-24 21:03:22 -0800

    Added files via upload

---
+++ HelloWorld3.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld3
+{
+	public static void main(String[] args){
+		String greeting;
+		greeting="Hello World!";
+	
+		//System.out.println(greeting);
+		
+		System.out.printf("%s\n",greeting);  // test printf
+	}
+}
