b8597ec49c2b685aab16fdf84f37d19da660f60d
yugi2004 <YUGI2004@GMAIL.COM>
2016-02-27 16:35:29 -0800

    Added files via upload

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.io.IOException;
+
 public class Calculate
 {
 	public static void main(String[] args)
@@ -12,62 +12,52 @@ public class Calculate
 	Scanner input= new Scanner(System.in);
 		System.out.print("Enter number 1:");
 		a=input.nextInt();
-		System.out.print("please enter \"+\" or \"-\" or \"*\" or \"/\" :");
-		x=input.next();// nhap chuoi
 		System.out.print("Enter number 2:");
 		b=input.nextInt();
-	while(x!="q")
-		{		
-		if (x.equals("+"))
-			{	KQ=a+b;
-				a=KQ;
-				System.out.printf("Result: %d\n",KQ);
-				System.out.print("please enter \"+\" or \"-\" or \"*\" or \"/\" or \"q\" ( Quit) :");
-				x=input.next();// nhap chuoi
-				System.out.print("Enter number next:");
-				b=input.nextInt();
-			}
-		else{
-				if  (x.equals("-"))
-				{
-					KQ=a-b;
+		System.out.print("please enter \"+\" or \"-\" or \"*\" or \"/\" or \"q\" ( Quit) :");
+		x=input.next();// nhap chuoi
+		
+			while(x!=null)
+			{		
+			if (x.equals("+"))
+				{	
+					KQ=a+b;
 					a=KQ;
 					System.out.printf("Result: %d\n",KQ);
-					System.out.print("please enter \"+\" or \"-\" or \"*\" or \"/\" or \"q\" ( Quit) :");
-					x=input.next();// nhap chuoi
-					System.out.print("Enter number next:");
-					b=input.nextInt();
+
 				}
-				else{
-						if  (x.equals("*"))
+				else if  (x.equals("-"))
+					{
+						KQ=a-b;
+						a=KQ;
+						System.out.printf("Result: %d\n",KQ);	
+					
+					}
+					else if  (x.equals("*"))
 						{	
 							KQ=a*b;
 							a=KQ;
 							System.out.printf("Result: %d\n",KQ);
-							System.out.print("please enter \"+\" or \"-\" or \"*\" or \"/\" or \"q\" ( Quit) :");
-							x=input.next();// nhap chuoi , input strin
-							System.out.print("Enter number next:");
-							b=input.nextInt();
 						}
-						else{
-								if  (x.equals("/"))// ham so sanh 2 chuoi , compare 2 String
+						else if  (x.equals("/"))// ham so sanh 2 chuoi , compare 2 String
 								{
 									KQ=a/b;
 									a=KQ;
 									System.out.printf("Result: %d\n",KQ);
-									System.out.print("please enter \"+\" or \"-\" or \"*\" or \"/\" or \"q\" ( Quit) :");
-									x=input.next();// nhap chuoi
-									System.out.print("Enter number next:");
-									b=input.nextInt();
+
 								}
-								else{
-									x="q";
-								}
-						}
-				}
+								else 
+									{
+										System.out.print("Bye!!!!");
+										break;
+									}
+					System.out.print("Enter number next:");
+					b=input.nextInt();
+					System.out.print("please enter \"+\" or \"-\" or \"*\" or \"/\" or \"q\" ( Quit) :");
+					x=input.next();// nhap chuoi
+
 			}
-			
-		}
+		
 	
 	}
 }
