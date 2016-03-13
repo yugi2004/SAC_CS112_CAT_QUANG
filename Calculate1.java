3b9d57de48ebc12c69489efb672462d4d9f2d471
yugi2004 <YUGI2004@GMAIL.COM>
2016-03-12 20:13:43 -0800

    Week 5 Homework

---
+++ Calculate1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Calculate1
+{
+	public static void main(String[] args)
+	{
+	     // TODO code application logic here
+        int a;
+		int b;
+		String x="";
+        String quit="N";
+	
+	Scanner input= new Scanner(System.in);
+        do{
+            System.out.print("Enter number 1:");
+		a=input.nextInt();
+		System.out.print("Enter number 2:");
+		b=input.nextInt();
+		System.out.print("please enter \"+\" or \"-\" or \"*\" or \"/\" :");
+		x=input.next();// nhap chuoi
+                        if(x.equals("+"))
+                    System.out.printf("Result: %d + %d = %d",a,b,a+b);
+                else if (x.equals("-"))
+                    System.out.printf("Result: %d - %d = %d",a,b,a-b);
+                     else if(x.equals("*"))
+                         System.out.printf("Result: %d * %d = %d",a,b,a*b);
+                        else if(x.equals("/"))
+                            System.out.printf("Result : %d / %d = %d", a,b,a/b);
+                
+                System.out.println("Do you want quit? Y or N");
+                quit=input.next();
+                if(quit.equalsIgnoreCase("y"))
+                    System.out.println("Thank you, Bye!");
+                
+                
+        }while(quit.equalsIgnoreCase("N"));
+		
+	
+	}
+}
