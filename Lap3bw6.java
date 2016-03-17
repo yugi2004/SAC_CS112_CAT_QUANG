99146d6c5a4d862b4854283897afbb7d4605f6bc
yugi2004 <YUGI2004@GMAIL.COM>
2016-03-16 21:17:59 -0700

    Lap 3b week6 use ? : operator

--- Lap3bw6.java
+++ Lap3bw6.java
@@ -6,24 +6,21 @@ public class Lap3bw6
 	public static void main(String[] args){
 		int computerNum;
 		int personNum;
-		int equalNum;
+		String tooBig="Too Big";
+		String tooSmall="Too Small";
+		
 		computerNum = 1 + (int) (Math.random () * 10); ///  random of math 
 		Scanner input= new Scanner(System.in);
 		System.out.println("Please enter your number:");
 		personNum=input.nextInt();
 		System.out.printf("Your number is : %d\n ",personNum);
 		System.out.printf("Computer number is : %d\n ",computerNum);
-		equalNum=(computerNum==personNum)? 1 : 2;
-		if(equalNum==2)
-		{
-			equalNum=(computerNum>personNum)? 1 : 2;
-			if(equalNum==1)
-				System.out.println("your number is too small");
-			else
-				System.out.println("your number is too big");
-
-		}
-		else
+		if(computerNum==personNum)
 			System.out.println("your number is correct");
+		else
+			{	
+				String messageS = (computerNum>personNum) ? tooBig : tooSmall;
+				System.out.println(messageS);
+			}
 	}
 }
