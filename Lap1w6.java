7e6539d5222a647c0dc90b61763e2f1f43407010
yugi2004 <YUGI2004@GMAIL.COM>
2016-03-16 19:45:43 -0700

    Lap1 week6

---
+++ Lap1w6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+// Lap 1  - Week6 
+public class Lap1w6
+{
+	public static void main(String[] args){
+		int computerNum;
+		int personNum;
+		computerNum = 1 + (int) (Math.random () * 10);
+		Scanner input= new Scanner(System.in);
+		System.out.println("Please enter your number:");
+		personNum=input.nextInt();
+		System.out.printf("Your number is : %d\n ",personNum);
+		if(computerNum==personNum)
+			System.out.println("your number is correct");
+		else if(computerNum>personNum)
+			System.out.println("your number is too small");
+			else 
+				System.out.println("your number is too big");
+		
+	}
+}
