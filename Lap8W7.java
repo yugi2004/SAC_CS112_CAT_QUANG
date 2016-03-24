01c039cb52518a5c4a030fe80926ffd11fde25c0
yugi2004 <YUGI2004@GMAIL.COM>
2016-03-23 21:53:54 -0700

    Lap8 Week7 Array

---
+++ Lap8W7.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+
+public class Lap8W7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] numArry= new int[]{123,456,479,135,246,135};
+		Arrays.sort(numArry);
+		
+		for(int i=0;i<numArry.length;i++)
+		{
+			System.out.printf("the nume of array is %d\n",numArry[i]);
+		}
+		
+	}
+
+}
