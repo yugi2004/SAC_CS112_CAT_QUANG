9ba0bca88c4725e5c42ff5b7d61436ca95ab6f90
yugi2004 <YUGI2004@GMAIL.COM>
2016-03-23 20:18:58 -0700

    Lap2 Week7 Rock/Paper/Scissors Flow Chart on PDF

---
+++ Lap2W7.java
@@ -0,0 +1,41 @@
+
+import javax.swing.JOptionPane;
+
+public class Lap2W7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String personPlay;//  input person play
+	    String computerPlay=null;    
+	    String message;
+	    int computerInt; // theo doi bien random cua computer
+	    
+	     
+	    // Scanner scan = new Scanner(System.in);
+	    
+	    personPlay=JOptionPane.showInputDialog(null,"Welcome to a rock-paper-scissor game!\nPlease enter your move :\nRock=R,Paper=P, and Scissor=S");
+        
+        // //radom computer's play (1,2,3)
+        
+        computerInt= 1+ (int)(Math.random()*3);   
+        if (computerInt ==1) 
+            computerPlay = "R"; 
+             else computerPlay=(computerInt==2)? "P": "S";
+        
+        message= String.format("Computer play is: %s\n your Choice is: %s\n  ",computerPlay,personPlay);
+        JOptionPane.showMessageDialog(null, message);
+     
+        //check who won. Use  if:
+        if (personPlay.equalsIgnoreCase(computerPlay))
+        	JOptionPane.showMessageDialog(null, "it is a tie!!!!!!");
+        else if ((personPlay.equalsIgnoreCase("S") && computerPlay.equalsIgnoreCase("R"))||(personPlay.equalsIgnoreCase("R") && computerPlay.equalsIgnoreCase("P"))||(personPlay.equalsIgnoreCase("P") && computerPlay.equalsIgnoreCase("S"))) 
+                 { 
+        			JOptionPane.showMessageDialog(null, "Computer Win !! You Lose!!");
+				   }	  
+        	 else
+        		 JOptionPane.showMessageDialog(null, "You Win !! Computer Lose!!");
+
+	}
+}
+
+
