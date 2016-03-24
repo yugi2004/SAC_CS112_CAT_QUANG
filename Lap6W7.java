aebdce853a01dbc3978829b1998812fd87735f79
yugi2004 <YUGI2004@GMAIL.COM>
2016-03-23 21:22:34 -0700

    Lap6 Week7 Game do while loop to allow user to play 3 times

---
+++ Lap6W7.java
@@ -0,0 +1,43 @@
+import javax.swing.JOptionPane;
+
+
+public class Lap6W7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int computerNum;
+		int personNum;
+		String message;
+		int time=1;
+		JOptionPane.showMessageDialog(null,"Your have 3 times play!!!");
+		do{			
+					
+		computerNum = 1 + (int) (Math.random () * 10); ///  random of math 
+		String var=JOptionPane.showInputDialog("Please enter your number:");// return type String
+		
+		// change String to int use Integer.parseInt(string) or Integer.valueof(string)
+		personNum = Integer.parseInt(var);
+		
+		// use String.format("") container your message ;
+		message=String.format("Your number is : %d\nComputer number is : %d\n",personNum,computerNum);
+		JOptionPane.showMessageDialog(null,message);
+		
+		if(computerNum==personNum)
+		{
+	
+			JOptionPane.showMessageDialog(null,"your number is correct!! You Won!!! ");
+		}
+		else
+			{	
+				String messageS = (computerNum<personNum) ? "your number is too big":"your number is too small";
+				JOptionPane.showMessageDialog(null,messageS);
+			}
+		message=String.format("You finished %d times",time);
+		JOptionPane.showMessageDialog(null,message);
+		time++;
+		}while(time<=3);
+		JOptionPane.showMessageDialog(null,"thank you ! Bye ! see you soon");
+
+	}
+
+}
