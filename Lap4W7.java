f7c2dd1be3b3c8ad5e862fa08387b7241a74678b
yugi2004 <YUGI2004@GMAIL.COM>
2016-03-25 17:00:26 -0700

    Lap4 Week7 use boolean

---
+++ Lap4W7.java
@@ -0,0 +1,34 @@
+import javax.swing.JOptionPane;
+
+public class Lap4W7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int computerNum;
+		int personNum;
+		String message;
+		
+		computerNum= 1 + (int) Math.random()*10;// random 1-10
+		String var= JOptionPane.showInputDialog("Please enter your number :");
+		// change String to Int
+		personNum=Integer.parseInt(var);
+		// use String.format("") container your message ;
+		message=String.format("Your number is : %d\nComputer number is : %d\n",personNum,computerNum);
+		JOptionPane.showMessageDialog(null,message);
+		
+		boolean tooBig=(computerNum<personNum);
+		boolean isEqual=(computerNum==personNum);
+		if(isEqual)
+		{
+				JOptionPane.showMessageDialog(null,"your number is correct!! You Won!!! ");
+		}
+		else
+			{	
+				String messageS = (tooBig) ? "your number is too big":"your number is too small";
+				JOptionPane.showMessageDialog(null,messageS);
+			}
+ 
+		JOptionPane.showMessageDialog(null,"thank you ! Bye ! see you soon");
+	}
+
+}
