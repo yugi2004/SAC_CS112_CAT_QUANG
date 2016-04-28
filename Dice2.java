49f00b4a026c618fe2932653865e1576aeef58ff
yugi2004 <YUGI2004@GMAIL.COM>
2016-04-27 21:11:21 -0700

    Static Method, static fields , how to use

---
+++ Dice2.java
@@ -0,0 +1,8 @@
+
+public class Dice2 {
+	private static int randomNumber;
+	public static int OneThrow(){
+		randomNumber =1+(int)(Math.random()*6);
+		return randomNumber;
+	}
+}
---
+++ DiceGame3.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+
+public class DiceGame3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Dice2.OneThrow());
+	}
+
+}
