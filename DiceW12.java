0d5327e89cfe947a730239d2e0f4d2508ddb4872
yugi2004 <YUGI2004@GMAIL.COM>
2016-05-04 19:55:43 -0700

    Lab 2  - Composition Create ThreeDice class with 3 Dice objects, D1, D2 and D3 Method Throw() Return average of 3 dices

--- DiceW12.java
+++ DiceW12.java
@@ -1,14 +1,19 @@
-//Lab 1 – Compile Time Polymorphism _Static Polymorphism
+//Lab 1 â€“ Compile Time Polymorphism _Static Polymorphism
 public class DiceW12 {
 	private static int randomNumber;
+	public static int Throw()
+	{
+		randomNumber=( (int)(Math.random()*6 ) + 1 );
+		return randomNumber;
+	}
 	public static int Throw(int dices)
 	{
-		
-		randomNumber=dices * ((int)(Math.random()*6)+1);
+		randomNumber=dices * ( (int)(Math.random()*6 ) + 1 );
 		return randomNumber;
 	}
 	public static int Throw(int dices, int bounces)
 	{
+		
 		randomNumber=dices * ((int)(Math.random()*6)+1) + bounces;
 		return randomNumber;
 	}
