a81854d236d79b8dd8b9bfc78d5b6574697f611c
yugi2004 <YUGI2004@GMAIL.COM>
2016-05-11 19:38:57 -0700

    Test the ToString() method of  ThreeDice class in the main program

---
+++ DiceW13.java
@@ -0,0 +1,20 @@
+//Lab 1 – Compile Time Polymorphism _Static Polymorphism
+public class DiceW13 {
+	private static int randomNumber;
+	public static int Throw()
+	{
+		randomNumber=( (int)(Math.random()*6 ) + 1 );
+		return randomNumber;
+	}
+	public static int Throw(int dices)
+	{
+		randomNumber=dices * ( (int)(Math.random()*6 ) + 1 );
+		return randomNumber;
+	}
+	public static int Throw(int dices, int bounces)
+	{
+		
+		randomNumber=dices * ((int)(Math.random()*6)+1) + bounces;
+		return randomNumber;
+	}
+}
