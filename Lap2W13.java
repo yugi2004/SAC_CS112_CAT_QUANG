2b9ffde717136b228f48b75ace8a2364b33dd34b
yugi2004 <YUGI2004@GMAIL.COM>
2016-05-11 19:41:30 -0700

    Test the ToString() method of  ThreeDice class in the main program

---
+++ Lap2W13.java
@@ -0,0 +1,11 @@
+
+public class Lap2W13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ThreeDice dice = new ThreeDice();
+		System.out.println(dice);
+		
+	}
+
+}
--- ThreeDice.java
+++ ThreeDice.java
@@ -1,10 +1,14 @@
 
 public class ThreeDice {
-	private  DiceW12 D1,D2,D3;
+	private  DiceW13 D1,D2,D3;
 	public  int Throw()
 	{
 		int randomDice;
 		randomDice = (D1.Throw()+D2.Throw()+D3.Throw())/3;			
 		return randomDice;
+	}
+	public String toString()
+	{
+		return String.format("Random number of 3 dice is : %d", Throw());
 	}
 }
