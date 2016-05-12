68bfa2c14d160bf24430fa7a63b5d4962d488344
yugi2004 <YUGI2004@GMAIL.COM>
2016-05-11 20:33:07 -0700

    Test the ToString() method of  ThreeDice class in the main program

--- ThreeDice.java
+++ ThreeDice.java
@@ -3,6 +3,9 @@ public class ThreeDice {
 	private  DiceW13 D1,D2,D3;
 	public  int Throw()
 	{
+		D1=new DiceW13();
+		D2=new DiceW13();
+		D3=new DiceW13();
 		int randomDice;
 		randomDice = (D1.Throw()+D2.Throw()+D3.Throw());			
 		return randomDice;
