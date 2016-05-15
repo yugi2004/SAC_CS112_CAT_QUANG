d473d92d84efe5841432fd42a06c165575a90e35
yugi2004 <YUGI2004@GMAIL.COM>
2016-05-14 20:45:43 -0700

    Implement the clone and equals method for the TrippleDice object Test the clone and equal method from main program

---
+++ HW13.java
@@ -0,0 +1,20 @@
+
+public class HW13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+		TrippleDice dice1=new TrippleDice();
+		TrippleDice dice2 = (TrippleDice)dice1.clone();
+		//clone is copy object # create new object
+		System.out.println(dice1);
+		System.out.println(dice2);
+		
+		System.out.println(dice1.equals(dice2));
+		}
+		catch(CloneNotSupportedException c)
+		{
+			System.err.println("Error.....");
+		}
+	}
+}
--- TrippleDice.java
+++ TrippleDice.java
@@ -1,5 +1,5 @@
-
-public class TrippleDice extends DiceW13{
+// class extend DiceW13 and Implement with Cloneable
+public class TrippleDice extends DiceW13 implements Cloneable{
 	private  int randomNumber;
 	@Override
 	public int Throw()
@@ -7,4 +7,21 @@ public class TrippleDice extends DiceW13{
 		randomNumber=super.Throw()+super.Throw()+super.Throw();
 		return randomNumber;
 	}
+	public int getNumber()
+	{
+		return randomNumber;
+	}
+	public boolean equals(TrippleDice obj)
+	{
+		if(this.getNumber()==obj.getNumber())return true;
+		else return false;
+	}
+	public String toString()
+	{
+		return String.format("Random number of Tripple Dice is :%d",Throw());
+	}
+	// khai bao object clone and handling 
+	public Object clone()throws CloneNotSupportedException{  
+		return super.clone();  
+		}  
 }
