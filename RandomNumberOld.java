a659c6c748a429fe0d2373035779ba93be7015ed
yugi2004 <YUGI2004@GMAIL.COM>
2016-05-18 21:13:51 -0700

    Create RandomNumberOld.java

---
+++ RandomNumberOld.java
@@ -0,0 +1,14 @@
+-public class RandomNumber {
+-	private int num=0;
+-	public RandomNumber()
+-	{
+-		num= 1 + (int) (Math.random()*10);
+-	}
+-	
+-	public int GetANumber_Between_1_and_10()
+-
+-	{		
+-		
+-		return num;
+-	}
+-	// thay the cho class randomnumber bi xoa cu
