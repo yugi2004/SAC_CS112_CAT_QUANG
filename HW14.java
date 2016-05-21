a0d34261ff5b090f8557fe2f9e6b716b74032c08
yugi2004 <YUGI2004@GMAIL.COM>
2016-05-20 21:32:12 -0700

    Automobile implement Drivable (or Drive interface if you'd prefer)  SportCar extends Automobile  MiniVan extends Automobile  Drivable arrays can hold any Automobile objects

---
+++ HW14.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class HW14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		AutomobileW14[] auto = new AutomobileW14[2];
+		auto[0]= new SportCar(2015,"Lamborghini" , "Gallardo ","Red","Spyder Performante", 10, 0.32);
+		auto[1]= new MiniVan(2016, "ToyoTa", "Sienna", "Yellow", "LE ", 7, 0.24);
+		int cycle=0;
+		for(AutomobileW14 currentauto : auto)
+		{
+
+			System.out.printf("%s %s :","Please input Operating Cycles of ",currentauto.getClass().getName());
+			cycle=input.nextInt();
+			currentauto.Drive(cycle);
+			for(int i =1 ; i <13; i ++)
+			{
+				currentauto.Drive(cycle);
+				System.out.printf("%.2f : %d ",currentauto.getSpeed(),currentauto.getGear());
+				System.out.println(currentauto);
+			}
+		}
+	}
+
+}
