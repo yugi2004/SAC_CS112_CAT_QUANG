56099a011035e5f428f106797cd01202e45beb2b
yugi2004 <YUGI2004@GMAIL.COM>
2016-05-25 20:36:55 -0700

    Convert existing Dice class to implement runnable interface Write a test program to throw the dice 25 times in separate threads

--- Lap1W15.java
+++ Lap1W15.java
@@ -6,15 +6,15 @@ public class Lap1W15 {
 		System.out.println("Start");
 		for(int i=0;i<2;i++)
 		{
-			Runnable T1 = new DiceW15(25);// 25 is times to throw t1 object 1
-			Thread W1 = new Thread(T1);// gan object cho Thread
+			Runnable T1 = new DiceW15(25);
+			Thread W1 = new Thread(T1);//  object to Thread
 			W1.setName(String.valueOf(10));
 		      // Start the thread, never call method run() direct
-			W1.start();    // se tu dong goi ham run() de thuc thi cau lenh
+			W1.start();    
 			
-			Runnable T2 = new DiceW15(35);// 35 is times to throw, t2 object 2
+			Runnable T2 = new DiceW15(35);
 			Thread W2 = new Thread(T2);
-			W2.setName(String.valueOf(20));// gan gia tri name cho luong du lieu
+			W2.setName(String.valueOf(20));//name to thread 2
 			W2.start();
 		}
 		System.out.println("Completed");
